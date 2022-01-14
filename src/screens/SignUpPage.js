dc84d57a8d747727c2a873af336215ec530ca9ba
akash <amitjaiswal1990@gmail.com>
2022-01-14 18:02:14 +0530

    Anand first code

---
+++ src/screens/SignUpPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, StyleSheet, Text, TextInput} from 'react-native';
+
+const SignUpPage = () => {
+  return (
+    <View>
+      <Text>This is the sign up page</Text>
+    </View>
+  );
+};
+
+export default SignUpPage;
